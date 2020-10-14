ecf9462585bc79023f7fccc9e06a6d0eba22ad7f
760483 <760483@SAIT224160IT.ACDM.DS.SAIT.CA>
2020-10-14 09:29:08 -0600

    Created project. Get to work.

---
+++ src/java/servlets/ShoppingListServlet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShoppingListServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        /*
+        Things to do:
+        Send the user to register if they clicked log out otherwise send them to
+        shoppingList
+        
+        */
+        
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+}
---
+++ web/WEB-INF/shoppingList.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : shoppingList
+    Created on : Oct 14, 2020, 9:26:07 AM
+    Author     : 760483
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
